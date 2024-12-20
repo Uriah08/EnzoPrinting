62c8690a51dffc4486940d258031f9c82e41245c
Uriah08 <lorenz08.flores@gmail.com>
2024-12-20 18:02:55 +0800

    UI clean: Dashboard UI

--- src/components/containers/AdminContainers/Dashboard.tsx
+++ src/components/containers/AdminContainers/Dashboard.tsx
@@ -2,7 +2,11 @@ import { Session } from 'next-auth'
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
-import { ShoppingBag } from 'lucide-react'
+import { Clock, ShoppingBag } from 'lucide-react'
+import { PieCharts } from '@/components/charts/profile-orders'
+import { BarChartX } from '@/components/charts/bar-chart-x'
+import { BarChartSticks } from '@/components/charts/bar-chart-x-sticks'
+import { RadialBarAdmin } from '@/components/charts/radial-chart'
 
 const Dashboard = ({ session }: {session?: Session | null}) => {
   return (
@@ -31,9 +35,9 @@ const Dashboard = ({ session }: {session?: Session | null}) => {
         }
       </div>
       <div className='w-full lg:h-full flex flex-col-reverse lg:flex-row gap-5 overflow-y-hidden'>
-        <div className='lg:w-2/3 h-[100vh] lg:h-full w-full bg-[#f5f5f5] rounded-lg shadow-lg p-5 flex flex-col gap-5'>
+        <div className='lg:w-2/3 h-[100vh] lg:h-full w-full bg-[#f5f5f5] rounded-lg shadow-lg p-5 flex flex-col gap-5 overflow-y-auto custom-scroll-bar'>
           <div className='flex flex-wrap gap-5'>
-          <div className='bg-main flex-1 p-5 rounded-lg flex flex-col gap-5'>
+          <div className='bg-main flex-1 p-5 rounded-lg flex flex-col gap-5 shadow-lg'>
             <div className='flex gap-3 items-center'>
               <ShoppingBag size={32} className='p-[6px] ml-1 shadow-md rounded-md bg-white text-main' />
               <h1 className='text-[#f5f5f5] text-xl font-semibold'>Product</h1>
@@ -42,7 +46,7 @@ const Dashboard = ({ session }: {session?: Session | null}) => {
               <h1 style={{ fontSize: "20px" }} className='font-bold text-[#f5f5f5]'>1233</h1>
             </div>
           </div>
-          <div className='bg-main flex-1 p-5 rounded-lg flex flex-col gap-5'>
+          <div className='bg-main flex-1 p-5 rounded-lg flex flex-col gap-5 shadow-lg'>
             <div className='flex gap-3 items-center'>
               <ShoppingBag size={32} className='p-[6px] ml-1 shadow-md rounded-md bg-white text-main' />
               <h1 className='text-[#f5f5f5] text-xl font-semibold'>Product</h1>
@@ -51,7 +55,7 @@ const Dashboard = ({ session }: {session?: Session | null}) => {
               <h1 style={{ fontSize: "20px" }} className='font-bold text-[#f5f5f5]'>1233</h1>
             </div>
           </div>
-          <div className='bg-main flex-1 p-5 rounded-lg flex flex-col gap-5'>
+          <div className='bg-main flex-1 p-5 rounded-lg flex flex-col gap-5 shadow-lg'>
             <div className='flex gap-3 items-center'>
               <ShoppingBag size={32} className='p-[6px] ml-1 shadow-md rounded-md bg-white text-main' />
               <h1 className='text-[#f5f5f5] text-xl font-semibold'>Product</h1>
@@ -60,7 +64,7 @@ const Dashboard = ({ session }: {session?: Session | null}) => {
               <h1 style={{ fontSize: "20px" }} className='font-bold text-[#f5f5f5]'>1233</h1>
             </div>
           </div>
-          <div className='bg-main flex-1 p-5 rounded-lg flex flex-col gap-5'>
+          <div className='bg-main flex-1 p-5 rounded-lg flex flex-col gap-5 shadow-lg'>
             <div className='flex gap-3 items-center'>
               <ShoppingBag size={32} className='p-[6px] ml-1 shadow-md rounded-md bg-white text-main' />
               <h1 className='text-[#f5f5f5] text-xl font-semibold'>Product</h1>
@@ -70,8 +74,111 @@ const Dashboard = ({ session }: {session?: Session | null}) => {
             </div>
           </div>
           </div>
+          <div className='grid md:grid-cols-2 grid-cols-1 gap-5'>
+            <PieCharts/>
+            <BarChartX/>
+          </div>
+          <BarChartSticks/>
         </div>
-        <div className='lg:w-1/3 h-full bg-[#f5f5f5] rounded-lg shadow-lg p-5'></div>
+        <div className='lg:w-1/3 h-full bg-[#f5f5f5] rounded-lg shadow-lg p-5 overflow-y-auto custom-scroll-bar'>
+        <div className='flex-1 bg-main p-5 rounded-lg flex flex-col gap-5 shadow-lg'>
+            <div className='flex gap-3 items-center'>
+              <Clock size={32} className='p-[6px] ml-1 shadow-md rounded-md text-main bg-[#f5f5f5]' />
+              <h1 className='text-[#f5f5f5] text-xl font-semibold'>Time</h1>
+            </div>
+            <div className='w-full my-5'>
+              <h1 className='text-center text-[#f5f5f5] text-xl font-semibold'>Jan 01, 2024</h1>
+              <h1 style={{ fontSize: "30px" }} className='font-bold text-[#f5f5f5] text-center'>Sun 12:30 AM</h1>
+            </div>
+          </div>
+
+          <RadialBarAdmin/>
+
+          <div className='flex flex-col'>
+            <h1 className='text-zinc-800 text-xl font-semibold'>Highlighted Product</h1>
+            <h1 className='text-zinc-500 text-sm'>Featured for its popularity and exceptional value.</h1>
+            <div className='flex flex-col mt-5 gap-3'>
+              <div className='flex gap-3'>
+                <Image src={'/products/mugsample1.jpg'} width={500} height={500} alt='profile' className='size-20'/>
+                <div className='flex justify-between w-full'>
+                <div className='flex flex-col'>
+                  <h1 className='text-xs bg-main py-[2px] px-3 rounded-full w-fit text-[#f5f5f5]'>book</h1>
+                  <h1 className='text-zinc-800 text-lg font-medium'>Quote Mug</h1>
+                </div>
+                <h1 className='text-xs font-semibold text-zinc-800'>Price: <span className='font-normal'>P 12342.00</span></h1>
+                </div>
+              </div>
+              <div className='flex gap-3'>
+                <Image src={'/products/mugsample1.jpg'} width={500} height={500} alt='profile' className='size-20'/>
+                <div className='flex justify-between w-full'>
+                <div className='flex flex-col'>
+                  <h1 className='text-xs bg-main py-[2px] px-3 rounded-full w-fit text-[#f5f5f5]'>book</h1>
+                  <h1 className='text-zinc-800 text-lg font-medium'>Quote Mug</h1>
+                </div>
+                <h1 className='text-xs font-semibold text-zinc-800'>Price: <span className='font-normal'>P 12342.00</span></h1>
+                </div>
+              </div>
+              <div className='flex gap-3'>
+                <Image src={'/products/mugsample1.jpg'} width={500} height={500} alt='profile' className='size-20'/>
+                <div className='flex justify-between w-full'>
+                <div className='flex flex-col'>
+                  <h1 className='text-xs bg-main py-[2px] px-3 rounded-full w-fit text-[#f5f5f5]'>book</h1>
+                  <h1 className='text-zinc-800 text-lg font-medium'>Quote Mug</h1>
+                </div>
+                <h1 className='text-xs font-semibold text-zinc-800'>Price: <span className='font-normal'>P 12342.00</span></h1>
+                </div>
+              </div>
+              <div className='flex gap-3'>
+                <Image src={'/products/mugsample1.jpg'} width={500} height={500} alt='profile' className='size-20'/>
+                <div className='flex justify-between w-full'>
+                <div className='flex flex-col'>
+                  <h1 className='text-xs bg-main py-[2px] px-3 rounded-full w-fit text-[#f5f5f5]'>book</h1>
+                  <h1 className='text-zinc-800 text-lg font-medium'>Quote Mug</h1>
+                </div>
+                <h1 className='text-xs font-semibold text-zinc-800'>Price: <span className='font-normal'>P 12342.00</span></h1>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-col mt-7'>
+            <h1 className='text-zinc-800 text-xl font-semibold'>Top Buyer</h1>
+            <h1 className='text-zinc-500 text-sm'>Recognized for consistent purchases and unwavering loyalty.</h1>
+            <div className='flex flex-col mt-5 gap-3'>
+              <div className='flex gap-3 items-center'>
+                <Image src={'/profile.png'} width={500} height={500} alt='profile' className='size-12 rounded-full'/>
+                <div className='flex justify-between w-full'>
+                <div className='flex flex-col'>
+                  <h1 className='font-medium text-zinc-800'>Username</h1>
+                  <h1 className='text-zinc-500 text-sm'>username@email.com</h1>
+                </div>
+                <h1 className='text-xs font-semibold text-zinc-800'>Total: <span className='font-normal'>P 12342.00</span></h1>
+                </div>
+              </div>
+              <div className='flex gap-3 items-center'>
+                <Image src={'/profile.png'} width={500} height={500} alt='profile' className='size-12 rounded-full'/>
+                <div className='flex justify-between w-full'>
+                <div className='flex flex-col'>
+                  <h1 className='font-medium text-zinc-800'>Username</h1>
+                  <h1 className='text-zinc-500 text-sm'>username@email.com</h1>
+                </div>
+                <h1 className='text-xs font-semibold text-zinc-800'>Total: <span className='font-normal'>P 12342.00</span></h1>
+                </div>
+              </div>
+              <div className='flex gap-3 items-center'>
+                <Image src={'/profile.png'} width={500} height={500} alt='profile' className='size-12 rounded-full'/>
+                <div className='flex justify-between w-full'>
+                <div className='flex flex-col'>
+                  <h1 className='font-medium text-zinc-800'>Username</h1>
+                  <h1 className='text-zinc-500 text-sm'>username@email.com</h1>
+                </div>
+                <h1 className='text-xs font-semibold text-zinc-800'>Total: <span className='font-normal'>P 12342.00</span></h1>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   )
