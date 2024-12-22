12a6de85563b13d3e67870c2f474fc425c2ff07b
Uriah08 <lorenz08.flores@gmail.com>
2024-12-22 19:49:34 +0800

    UI clean: Product Page Highlights

--- src/app/(protected)/product/page.tsx
+++ src/app/(protected)/product/page.tsx
@@ -7,7 +7,7 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 import { ShoppingCart } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { useGetProductQuery, useCreateCartMutation, useGetCartQuery } from '@/store/api'
+import { useGetProductQuery, useCreateCartMutation, useGetCartQuery, useGetHighlightProductQuery } from '@/store/api'
 import { Skeleton } from '@/components/ui/skeleton'
 import { Dialog, DialogClose, DialogContent, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 
@@ -57,6 +57,9 @@ const ProductPage = () => {
       });
     
     const { data, isLoading } = useGetProductQuery();
+
+    const { data: highlight, isLoading: highlightLoading } = useGetHighlightProductQuery()
+    const highlights = highlight?.product || []
 
     const products = data?.product || []
 
@@ -147,6 +150,114 @@ const ProductPage = () => {
                     <h1 key={nav.label} onClick={() => setActive(nav.label)} className={`text-xs lg:text-lg font-light cursor-pointer px-1 sm:px-5 sm:py-1 rounded-sm ${active === nav.label ? 'text-[#f5f5f5] bg-main':'text-zinc-400'}`}>{nav.label}</h1>
                 ))}
             </div>
+            <h1 className='text-sm sm:text-xl font-semibold text-zinc-600 mt-10'>Highlighted Products</h1>
+            <div className='grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-5 mt-5'>
+                {highlightLoading ? (
+                    <>
+                    <Skeleton className='w-full h-[340px] rounded-lg'/>
+                    <Skeleton className='w-full h-[340px] rounded-lg'/>
+                    <Skeleton className='w-full h-[340px] rounded-lg'/>
+                    <Skeleton className='w-full h-[340px] rounded-lg'/>
+                    </>
+                ) : (
+                    highlights.length === 0 ? (
+                        <div className='absolute flex items-center justify-center text-2xl font-semibold text-zinc-500 w-full'>No Products Found</div>
+                    ) : (
+                        highlights.map((product) => (
+                            <div key={product.id} className='flex flex-col h-fit p-1 sm:p-3 border border-zinc-300 gap-4'>
+                                <Image src={product.image} width={500} height={500} alt='image' className='h-20 lg:h-72 sm:h-32 object-cover w-full object-center'/>
+                                <h1 className='px-2 py-1 rounded-full bg-main text-[#f5f5f5] text-xs w-fit'>{product.category}</h1>
+                                <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center'>
+                                    <h1 className='text-sm lg:text-xl font-semibold'>{product.name}</h1>
+                                    <h1 className='text-xs lg:text-sm text-zinc-700'>₱ {product.price}.00</h1>
+                                </div>
+                                <Dialog>
+                                    <DialogTrigger asChild>
+                                    <Button disabled={product.status === 'out of stock' || product.status === 'maintenance'} onClick={handleReset} className='bg-main hover:bg-main2'>
+                                        {product.status === 'out of stock' ? 'Out of Stock' : product.status === 'maintenance' ? 'Not Available' : 'Inquire'}
+                                    </Button>
+                                    </DialogTrigger>
+                                    <DialogContent aria-describedby={undefined}>
+                                        <DialogTitle className='text-center'>Product Information</DialogTitle>
+                                        <Form {...form}>
+                                        <form onSubmit={form.handleSubmit((values) => onSubmit(values, product.id, quantity))}>
+                                        <div className='flex flex-col gap-3 mt-5'>
+                                            <div className='flex gap-3'>
+                                            <Image src={product.image} width={500} height={500} alt='product image' className='size-36 object-cover'/>
+                                            <div className='flex flex-col justify-between'>
+                                                <div className='flex flex-col gap-3'>
+                                                <h1 className='text-xl font-semibold text-zinc-700'>{product.name}</h1>
+                                                <h1 className='text-xs px-3 py-1 bg-main rounded-full text-[#f5f5f5] w-fit'>{product.category}</h1>
+                                                </div>
+                                                <h1>₱ {product.price}.00</h1>
+                                            </div>
+                                            </div>
+                                        <p className='text-sm indent-10'>{product.description}</p>
+                                        <FormField
+                                                control={form.control}
+                                                name="facebook"
+                                                render={({ field }) => (
+                                                    <FormItem className='mt-5'>
+                                                    <FormLabel>Your Facebook Profile Link</FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="Add link here..." {...field} className='resize-none'/>
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                                />
+                                        <FormField
+                                                control={form.control}
+                                                name="description"
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                    <FormLabel>Customize</FormLabel>
+                                                    <FormControl>
+                                                        <Textarea placeholder="Add text here..." {...field} className='resize-none'/>
+                                                    </FormControl>
+                                                    <FormDescription>
+                                                        Tell the admin what you want to add or revise.
+                                                    </FormDescription>
+                                                    <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                                />
+                                                <div className='flex justify-between'>
+                                                    <div className='flex flex-col'>
+                                                        <h1 className='text-sm'>Product Quantity:</h1>
+                                                        <div className='flex gap-3 items-center mt-2'>
+                                                        <button type="button" onClick={() => setQuantity((prev) => (prev + 1))} className='size-6 flex items-center justify-center text-[#f5f5f5] rounded-sm bg-main hover:bg-main2'>+</button>
+                                                        <h1 className='w-[30px] text-center'>{quantity}</h1>
+                                                        <button type="button" onClick={() => setQuantity((prev) => Math.max(prev - 1, 1))} className='size-6 flex items-center justify-center text-[#f5f5f5] rounded-sm bg-main hover:bg-main2'>-</button>
+                                                    </div>
+                                                    </div>
+                                                    <h1 className='self-end'><span className='text-lg font-semibold'>Total:</span> ₱ {Number(product.price || 0) * quantity}.00</h1>
+                                                </div>
+                                        </div>
+                                        {!session ? (
+                                            <Link href={'/auth/sign-in'} className='w-full mt-5'>
+                                            <Button className='text-[#f5f5f5] bg-main hover:bg-main2 px-3 rounded-full w-full mt-5'>LOGIN</Button>
+                                            </Link>
+                                        ) : (
+                                            <div className='flex w-full justify-end gap-3 mt-5'>
+                                                <DialogClose asChild>
+                                                <Button type="button" className='bg-[#f5f5f5] hover:bg-[#dddcdc] text-black'>Check Out</Button>
+                                                </DialogClose>
+                                                <DialogClose asChild>
+                                                <Button type='submit' disabled={cartLoading} className='text-[#f5f5f5] bg-main hover:bg-main2'>{cartLoading ? 'Loading...':'Add to Cart'}</Button>
+                                                </DialogClose>
+                                            </div>
+                                        )}
+                                        </form>
+                                        </Form>
+                                    </DialogContent>
+                                </Dialog>
+                            </div>
+                        ))
+                    )
+                )}
+        </div>
+        <h1 className='text-sm sm:text-xl font-semibold text-zinc-600 mt-10'>Products</h1>
             <div className='grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-5 mt-5'>
                 {isLoading ? (
                     <>
